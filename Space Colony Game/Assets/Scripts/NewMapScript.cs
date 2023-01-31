5ce7e2fd0b748ba6811f9a99a5774adc417fb567
seamusom <55447839+seamusom@users.noreply.github.com>
2023-01-31 15:34:55 +0000

    sum stuff

---
+++ Space Colony Game/Assets/Scripts/NewMapScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMapScript : MonoBehaviour
+{
+    private Transform ship;
+    private Rigidbody2D shipRB;
+    private RandomArea rA;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rA = FindObjectOfType<RandomArea>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ship = collision.gameObject.transform;
+        shipRB = collision.gameObject.GetComponent<Rigidbody2D>();
+
+        if (shipRB.velocity.x < 0.1f)
+        {
+            float xPos = ship.position.x;
+            xPos *= -1;
+            ship.position = new Vector2(xPos,ship.position.y);
+        }
+
+        if (shipRB.velocity.x > 0.1f)
+        {
+            float xPos = ship.position.x;
+            xPos *= 1;
+            ship.position = new Vector2(xPos, ship.position.y);
+        }
+
+        //rA.SpawnPlanet();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

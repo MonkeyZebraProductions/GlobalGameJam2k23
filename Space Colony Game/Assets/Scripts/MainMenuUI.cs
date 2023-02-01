054befebca6f790b2b691b593f0329f20d9aec2e
seamusom <55447839+seamusom@users.noreply.github.com>
2023-02-01 11:50:34 +0000

    Main Menu UI

---
+++ Space Colony Game/Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public string zen;
+    public string normal;
+    public string hard;
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadSceneZen()
+    {
+        SceneManager.LoadScene(zen);
+    }
+
+    public void LoadSceneNormal()
+    {
+        SceneManager.LoadScene(normal);
+    }
+
+    public void LoadSceneHard()
+    {
+        SceneManager.LoadScene(hard);
+    }
+
+}
